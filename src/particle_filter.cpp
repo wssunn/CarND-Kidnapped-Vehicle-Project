4a3aa71264c6b50b5ae3678ffeb7068d8c80bb71
Ning Sun <ningsunprc@gmail.com>
2019-08-22 15:15:48 +0800

    debug

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -32,7 +32,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    *   from GPS) and all weights to 1. 
    * Add random Gaussian noise to each particle.
    */
-  num_particles = 1000;
+  num_particles = 100;
   normal_distribution<double> dist_x(x, std[0]);
   normal_distribution<double> dist_y(y, std[1]);
   normal_distribution<double> dist_theta(theta, std[2]);
